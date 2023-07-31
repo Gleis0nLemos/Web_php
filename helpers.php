20e033b084b72d210909d9263f15103bfe60afcf
Gleison Lemos <gleison04lemos@gmail.com>
2023-07-31 18:51:07 -0300

    feat: learning control structures

--- helpers.php
+++ helpers.php
@@ -1,8 +1,20 @@
 <?php
-// Arquivo de funções
+//Arquivo de funções
 
 function saudacao (): string {
-    return '<h1>Boa tarde</h1>';
+
+    $hora = 12;
+    $saudacao = '';
+
+    if($hora >= 0 && $hora <=5){
+        $saudacao = '<h1>Boa madrugada</h1>';
+    }
+
+    if($hora >= 6 && $hora <=12){
+        $saudacao = '<h1>Bom dia</h1>';
+    }
+
+    return $saudacao;
 };
 
 # a declaração 'string $texto' espera que o conteúdo de texto seja uma string
