e8b371b8fbe5f5fa31d251385769320992f52b8a
Gleison Lemos <gleison04lemos@gmail.com>
2023-08-09 23:33:12 -0300

    chore: add configs

--- sistema/config.php
+++ sistema/config.php
@@ -1,8 +1,15 @@
 <?php
 // Arquivo de configuração do sistema
 
-//echo 'Arquivo de configurações';
-
-
 # Definição do fuso horário padrão
 date_default_timezone_set('America/Sao_Paulo');
+
+#informações do sistema                                         
+define('SITE_NOME', 'Unset');
+define('SITE_DESCRICAO', 'Unset - Tecnologia em Sistemas');
+
+#urls do sistema
+define('URL_PRODUCAO', 'https://unset.com.br');
+define('URL_DESENVOLVIMENTO', 'htpp://lcoalhost/blog');
+        #poderia ser const SITE_NOME - 'Unset'
+        #porém não pode usar const em uma condicional
